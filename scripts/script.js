cac180a8da5199f1b3e47a21394ded1044716d1f
AlexForget <alexandreqc26@gmail.com>
2022-02-16 08:42:16 -0500

    Création de la grille

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
 /* ----- DÉCLARATION DES VARIABLES ----- */
 
 /* ----- DÉCLARATIOM DES DOCUMENT QUERY ----- */
-const test = document.querySelector("p");
+var container = document.querySelector("#container");
 
 /* ----- LOADING INITIAL ----- */
-window.onload = (e) => {
-  test.textContent = "Changement JS";
-};
+window.onload = (e) => {};
+
+for (let i = 0; i < 16; i++) {
+  const cellule = document.createElement("div");
+  cellule.classList.add("cellule");
+  container.appendChild(cellule);
+}
