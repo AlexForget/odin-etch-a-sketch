44829f768b55f5354f9af054b9c3cf1696f8db85
AlexForget <alexandreqc26@gmail.com>
2022-02-16 09:14:48 -0500

    Ajout des eventListener pour le changement de couleur d'une cellule de la grille

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,17 @@ var container = document.querySelector("#container");
 /* ----- LOADING INITIAL ----- */
 window.onload = (e) => {};
 
+/* ----- FUNCTIONS ----- */
+
+const mouseOver = (e) => {
+  console.log(e.target);
+  e.target.style.backgroundColor = "red";
+};
+
+// Création de la grille et de l'event listener
 for (let i = 0; i < 16; i++) {
   const cellule = document.createElement("div");
   cellule.classList.add("cellule");
+  cellule.addEventListener("mouseover", mouseOver);
   container.appendChild(cellule);
 }
